59ea772fcc8832849267059162068ab661bdd5f1
Marcos Allan <marcosallanm1to@gmail.com>
2024-11-15 11:38:53 -0300

    Game repository, service, controller

---
+++ src/main/java/com/omarcosallan/dslist/controllers/GameController.java
@@ -0,0 +1,23 @@
+package com.omarcosallan.dslist.controllers;
+
+import com.omarcosallan.dslist.dto.GameMinDTO;
+import com.omarcosallan.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
